5ccd8e7ea4405eb200000238c192cbb95902a37d
RimRaider639 <madhurimapoddar639@gmail.com>
2023-05-03 14:56:16 +0530

    backend init

---
+++ edit.js
@@ -0,0 +1,259 @@
+// let allimg = {
+//     powder : ['https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/cropped/ofiovu5ebel7daicgd0t/gnc-pro-performance-creatine-monohydrate-3000mg-powder-unflavoured.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/yv3rqqcmdyjlycajye7d/optimum-nutrition-on-gold-standard-100-whey-protein-isolate-powder-double-rich-chocolate.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/77a6f25d6ff74a9092010a342636c132/tata-go-fit-plant-protein-for-women-cafe-mocha.jpg',
+// 'https://onemg.gumlet.io/images/h_150,f_auto,q_auto,c_fit,w_150/hnn7c8flhjc0uaa0ntaz/complan-nutrition-and-health-drink-royale-chocolate.jpg',
+// 'https://onemg.gumlet.io/images/h_150,f_auto,q_auto,c_fit,w_150/rddjrx5bg3uoa6iz51fq/prohance-d-diabetes-nutritional-supplement-for-dietary-management-vanilla.jpg',
+// 'https://onemg.gumlet.io/images/q_auto,c_fit,w_150,h_150,f_auto/wkmxonjuot7skrmdoxxx/protinex-health-and-nutritional-drink-tasty-chocolate.jpg',
+// 'https://onemg.gumlet.io/images/f_auto,q_auto,c_fit,h_150,w_150/cropped/dfkev9fo45bbcbs63ndw/grd-powder-chocolate.png',
+// 'https://onemg.gumlet.io/images/h_150,q_auto,f_auto,w_150,c_fit/z9wjmlwmame4mzupbbzo/glucon-d-instant-energy-health-drink-tangy-orange.jpg'],
+//     tablets : ['https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/acc13a0749ba48c7a4275e795067bafb/centrum-men-supports-overall-health-veg-world-s-no.1-multivitamin.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/gc3u9gfc331wtsipnljl/tata-1mg-calcium-vitamin-d3-zinc-magnesium-and-alfalfa-tablets-for-joint-health-immunity.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/jpr9uwzjgcnvmspmqwgp/neurobion-forte-tablet.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/bb873216-e272-483e-8230-d7efd3a1896e/shelcal-500-tablet.jpeg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/zaoniru9woncxf4acu5z/supradyn-daily-multivitamin-tablet-with-essential-zinc-12-vitamins-5-trace-elements-for-daily-immunity-energy.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/twpuzix7nrwivbtg0n80/healthkart-hk-vitals-biotin-tablet.jpg'],
+//     capsules : ['https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/fwram2p7rbkoigjg6zas/tata-1mg-salmon-omega-3-fish-oil-capsule.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/olumjmbboursowikpgh9/tata-1mg-vitamin-b-complex-capsules.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/xkg2uukifmrwghktvquj/evion-400mg-capsule.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/cropped/kjkqva2kdbkqwawswcu4/becosules-capsule.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/73c2b656962348769f7f93a72090dcaa/tata-1mg-hair-skin-nails-supreme-collagen-biotin-zinc-iron-and-vitamin-b-capsule.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,w_150,h_150,q_auto,f_auto/cropped/wpk4ezfkumzcxjtco6id/fefol-z-capsule.jpg'],
+//     strip : ['https://www.1mg.com/otc/fri-john-tablet-10-each-otc736227',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/20ec713df1364bfc96878f66acd6dc26/body-first-my-strips-sleep-strip-natural-lavender.jpg'],
+//     syrup : ['https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/hapkrixlylzsdf9r1hci/ostocalcium-b12-banana-syrup.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/cropped/ofrnhsljkdfcmjuqyogv/zincovit-cl-syrup.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/qaqxed3ikulpjqe5x78z/polybion-lc-mango-syrup.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/xfpqx40nwo0rfamcaequ/livogen-adult-tonic.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/cropped/acx9wzyrkxdytfeiusig/r.b-tone-kid-syrup.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/cropped/hqxbklrjzwbhuul5pcja/dabur-restora-syrup.jpg'],
+//     bag : ['https://onemg.gumlet.io/images/h_150,q_auto,f_auto,w_150,c_fit/cropped/yslc7gvu7pgnmjwkyzo6/himalaya-wellness-green-tea-sachet-classic.png',
+// 'https://onemg.gumlet.io/images/h_150,q_auto,f_auto,w_150,c_fit/zztxxk5kpsgowlifcjpp/organic-india-tulsi-green-tea-lemon-ginger.jpg',
+// 'https://onemg.gumlet.io/images/h_150,q_auto,f_auto,w_150,c_fit/ngoqyzvswfzetmlupv7d/tetley-green-tea-bag-immune-with-added-vitamin-c-1.4gm-each-lemon-honey.jpg'],
+//     liquid : ['https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/cropped/xyq23ch6ms0lrtz5mziw/zincofer-liquid.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/ec83d6d8f7a54b36b6574e0dca39d983/healthkart-apple-cider-vinegar-natural-vinegar.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/o9jjudy40yrctnfo1peb/nourishvitals-apple-cider-vinegar-with-mother-vinegar-acidity-5.jpg',
+// 'https://onemg.gumlet.io/images/w_150,f_auto,h_150,q_auto,c_fit/d4f64880fdbd46aa8a3be1c4ccebb527/healthvit-apple-cider-vinegar-with-mother-vinegar-unfiltered.jpg'],
+//     default : ['https://onemg.gumlet.io/images/q_auto,h_150,w_150,c_fit,f_auto/cropped/epdft2hdjosihgy3onxj/as-it-is-nutrition-whey-protein-concentrate.jpg',
+// 'https://onemg.gumlet.io/images/h_150,c_fit,q_auto,f_auto,w_150/42e28e4351284a179779113d7db06bba/centrum-kids-supports-overall-health-veg-world-s-no.1-multivitamin.jpg',
+// 'https://onemg.gumlet.io/images/h_150,c_fit,q_auto,f_auto,w_150/d3bdggbwhxhtpszgsit8/nutrazee-complete-multivitamin-gummies-for-kids-adults-orange.jpg',
+// 'https://onemg.gumlet.io/images/h_150,c_fit,q_auto,f_auto,w_150/d0qtkv8wq5mr3gmlrofm/nature-sure-sharp-eye-sight-gummy.jpg',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/hruygy5lw0ivyqenv9ev/seven-seas-original-cod-liver-oil-capsule.jpg',
+// 'https://onemg.gumlet.io/images/f_auto,c_fit,q_auto,w_150,h_150/cropped/i5uvg3gnwteqkhjl3zuu/pure-nutrition-cold-pressed-raw-virgin-coconut-oil.png',
+// 'https://onemg.gumlet.io/images/c_fit,h_150,w_150,q_auto,f_auto/cropped/mf7djuoam08pb8qrc94z/pure-nutrition-hand-sanitizer.png']
+// }
+
+
+
+const fs = require('fs');
+let data;
+let original;
+// let store={};
+// fs.readFile('./prev_db.json', 'utf8', (err, jsonString) => {
+//     if (err) {
+//         console.log("File read failed:", err)
+//         return
+//     } 
+//     original = JSON.parse(jsonString)
+// })
+fs.readFile('./db.json', 'utf8', (err, jsonString)=> {
+    if (err) {
+        console.log("File read failed:", err)
+        return
+    }
+    data = JSON.parse(jsonString)
+    data.products.forEach((prod, i) => {
+        if (prod.Name.includes("Women")||prod.Name.includes("Female")){
+            prod.gender = "Women"
+        } else if (prod.Name.includes("Men")||prod.Name.includes("Male")){
+            prod.gender = "Men"
+        } else {
+            prod.gender = "Unisex"
+        }
+        // if (prod.rating!=="Nan"){
+        //     prod.rating = +prod.rating
+        // }
+        // prod.discount = Math.round((1-prod.mrp/prod.price)*100)
+        // if (prod.price == 0){
+        //     // prod.discount = 0
+        //     prod.price = +original.products[i].price.slice(1)
+        //     prod.mrp = +prod.price
+        //     // console.log(original[i])
+        
+        // } 
+        // else{
+        //     // prod.discount = ((1-(+prod.mrp.slice(1)/+prod.price.slice(1)))*100).toFixed(0)
+        //     prod.price = +prod.price.slice(1)
+        //     prod.mrp = +prod.mrp.slice(1)
+        // }
+        
+        // prod.stock = Math.floor(Math.random()*100)
+        // prod.brand = prod.Name.split(' ')[0]
+        // prod.id = (Math.random()*10000).toString()+prod.brand
+
+        // let x = prod.pack_size.split(' ')
+        // prod.type = x[x.length-1]
+
+        // let key = prod.type.trim().toLowerCase()
+        // let found = false
+        // if (store[key]){
+        //     store[key]+=1
+        // } else{
+        //     store[key]=1
+        // }
+
+        // console.log(key, typeof(allimg[key]))
+    //     for (let i in allimg){
+    //         if (key.includes(i)){
+    //             prod.image = allimg[i][Math.floor(allimg[i].length*Math.random())]
+    //             // console.log(key, i)
+    //             found=true
+    //             break;
+    //         }
+    //     }
+    //    if (!found){
+    //         prod.image = allimg.default[Math.floor(allimg.default.length*Math.random())]
+    //         // console.log(key)
+    //    }
+
+        
+    })
+    // console.log(store)
+    data = JSON.stringify(data)
+    fs.writeFile('./db.json', data, err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    })
+})
+
+
+
+
+// let brands = ["Tata 1mg", 
+// "HealthKart",
+// "Carbamide Forte",
+// "HealthVit",
+// "Nutrition Planet",
+// "Zingavita",
+// "Himalayan Organics",
+// "Oziva",
+// "Adorreal",
+// "GNC",
+// "RedVit",
+// "Maxtrich",
+// "Nutrabud",
+// "Nutrapure",
+// "Pure Nutrition",
+// "PureFoods",
+// "Revital",
+// "TrueBasics",
+// "Zeelab",
+// "Zincovit",
+// "Fitspire",
+// "Lycobact",
+// "Muscle Dominator"]
+
+// const sub_category = [
+//     "Women",
+//     "Men",
+//     "Kids",
+//     "Age 50+",
+//     "Vegetarian",
+//     "Sports"
+// ]
+
+// let types = {
+//   tablets: 6231,
+//   capsules: 4315,
+//   solution: 96,
+//   powder: 1381,
+//   granules: 180,
+//   captabs: 2,
+//   gummies: 471,
+//   bottles: 291,
+//   syrup: 1187,
+//   drops: 145,
+//   liquid: 154,
+//   suspension: 276,
+//   drop: 141,
+//   cr: 12,
+//   er: 2,
+//   serum: 7,
+//   sr: 26,
+//   soflets: 8,
+//   tablet: 205,
+//   ir: 1,
+//   kit: 28,
+//   sachet: 45,
+//   vegicaps: 448,
+//   sachets: 101,
+//   packs: 119,
+//   lotion: 46,
+//   tonic: 33,
+//   softules: 5,
+//   bottle: 53,
+//   gel: 23,
+//   dilution: 327,
+//   strips: 47,
+//   paste: 8,
+//   solutions: 2,
+//   soap: 1,
+//   vials: 7,
+//   nuts: 4,
+//   diskette: 1,
+//   bar: 8,
+//   caplets: 40,
+//   candies: 1,
+//   spray: 26,
+//   oil: 18,
+//   seeds: 2,
+//   jar: 81,
+//   syrups: 3,
+//   gummy: 8,
+//   churna: 4,
+//   oats: 4,
+//   juice: 7,
+//   globules: 95,
+//   lozenges: 9,
+//   gums: 7,
+//   sanitizer: 8,
+//   cream: 41,
+//   elixir: 3,
+//   muesli: 2,
+//   md: 12,
+//   dr: 1,
+//   pack: 32,
+//   butter: 15,
+//   masks: 2,
+//   wash: 3,
+//   shampoo: 1,
+//   scrub: 2,
+//   ointment: 6,
+//   salt: 1,
+//   biscuits: 1,
+//   dt: 9,
+//   strip: 1,
+//   capsule: 6,
+//   pellets: 5,
+//   bags: 3,
+//   tr: 9,
+//   boxes: 5,
+//   combikit: 1,
+//   jelly: 6,
+//   bars: 5,
+//   emulsion: 1,
+//   diskettes: 2,
+//   candy: 1,
+//   powders: 4,
+//   gargle: 1,
+//   servings: 1,
+//   flakes: 2,
+//   tubes: 1,
+//   xl: 2,
+//   gels: 1,
+//   microgranules: 6
+// }
+
